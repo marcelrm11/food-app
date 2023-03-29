e32ac98c512306b23ce73ed81148ac714383510d
marcelrm11 <marcelrm11@gmail.com>
2023-03-29 18:12:07 +0200

    Add a new recipe page that fetches recipe information from Spoonacular API based on the id in the URL and displays the recipe title, summary, image, ingredients, diets, and wine pairings

---
+++ src/pages/recipe/[id].tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+export default function Recipe() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [info, setInfo] = useState(null);
+  useEffect(() => {
+    const fetchInfo = async () => {
+      const response = await fetch(
+        `https://api.spoonacular.com/recipes/${id}/information`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "x-api-key": `${process.env.API_KEY}`,
+          },
+        }
+      );
+      const data = await response.json();
+      console.log("recipe info:", data);
+      setInfo(data);
+    };
+    fetchInfo();
+  }, [id]);
+  if (info) {
+    return (
+      <article>
+        <h2>{info.title}</h2>
+        <p dangerouslySetInnerHTML={{ __html: info.summary }}></p>
+        <img src={info.image} alt={info.title} />
+        <h4>Ingredients:</h4>
+        <ul>
+          {info.extendedIngredients.map((ing) => {
+            return (
+              <li key={ing.id}>
+                <h6>{ing.name}</h6>
+              </li>
+            );
+          })}
+        </ul>
+        <h4>Diets:</h4>
+        <p>{info.diets.join(", ")}</p>
+        <h4>Pairings:</h4>
+        <p>{info.winePairing.pairingText}</p>
+      </article>
+    );
+  }
+}
