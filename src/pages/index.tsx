7e6a997ac97f409c29e19311aecfab4fa6b91f3f
marcelrm11 <marcelrm11@gmail.com>
2023-03-29 18:11:39 +0200

    Add website favicon and remove console log in index.tsx file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,6 @@ export default function Home() {
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    console.log(process.env.API_KEY);
     const fetchRecipes = async () => {
       const response = await fetch(
         "https://api.spoonacular.com/recipes/complexSearch",
@@ -35,7 +34,6 @@ export default function Home() {
           content="Find recipes with the ingredients you have at home. Track what is about to expire and what you need to buy."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/marcel.ico" />
       </Head>
       <main className={inter.className}>
         <h1>My Food App</h1>
